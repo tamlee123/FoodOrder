95a458ade943526e651307e59748730f142f74d8
tam <cherykim123@gmail.com>
2021-06-22 16:16:31 -0700

    working with ref and forward ref + adding cartReducer andapply useContext

--- src/components/Layout/CartButtonHeader.js
+++ src/components/Layout/CartButtonHeader.js
@@ -1,15 +1,20 @@
-import React from "react";
+import { useContext } from "react";
+import CartContext from "../../store/cartContext";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./CartButtonHeader.module.css";
 
 const CartButtonHeader = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItem = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClickShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItem}</span>
     </button>
   );
 };
