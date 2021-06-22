18456371c9c4049f8c13674257c1624ef7aa019d
tam <cherykim123@gmail.com>
2021-06-22 14:53:52 -0700

    creating CartContext and CartProvider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/cartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -12,14 +13,14 @@ function App() {
     setCartIsShown(false);
   };
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onCloseCart={hideCartHandler} />}
 
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
