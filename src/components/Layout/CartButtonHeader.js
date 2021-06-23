f2b8a8754f6d89023e7f7b857093c7947702643a
tam <cherykim123@gmail.com>
2021-06-22 23:06:31 -0700

    implement add and remove item in cart & hightlighted btn

--- src/components/Layout/CartButtonHeader.js
+++ src/components/Layout/CartButtonHeader.js
@@ -1,15 +1,30 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cartContext";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./CartButtonHeader.module.css";
 
 const CartButtonHeader = (props) => {
+  const [btnHighlighted, setBtnHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
   const numberOfCartItem = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+  const { items } = cartCtx;
+  const btnClasses = `${classes.button} ${btnHighlighted ? classes.bump : ""}`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={classes.button} onClick={props.onClickShowCart}>
+    <button className={btnClasses} onClick={props.onClickShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
